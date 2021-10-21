b5fcb12e14a897ad9920bcb0f1445555a2fa4b7e
YuSTerekhina <loneliness2004@mail.ru>
2021-10-22 02:15:50 +0300

    Eighth task done and fixed seventh task (accept an array in the method from main)

--- src/main/java/ru/gb/HomeWork03.java
+++ src/main/java/ru/gb/HomeWork03.java
@@ -45,7 +45,22 @@ public class HomeWork03 {
         System.out.println("----------------------------");
         System.out.println("Задание №7");
         System.out.println("----------------------------");
-        System.out.println(sumArray());
+        int arr1[] = {2, 2, 2, 1, 2, 2, 10, 1};
+        int arr2[] = {2, 2, 0, 1, 2, 2, 1, 1, 2};
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(sumArray(arr1));
+        System.out.println(Arrays.toString(arr2));
+        System.out.println(sumArray(arr2));
+
+        System.out.println();
+        System.out.println("----------------------------");
+        System.out.println("Задание №8");
+        System.out.println("----------------------------");
+        //int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};  не смогла разобраться как на вход в метод offsetArr передать массив
+        //System.out.println(Arrays.toString(arr));
+        //System.out.println(offsetArr(arr, 3));
+        offsetArr();
+
     }
 
     public static void changesNum() {
@@ -142,27 +157,16 @@ public class HomeWork03 {
         System.out.println("Максимальный элемент массива = " + max + "\nМинимальный элемент массива = " + min);
     }
 
-    public static boolean sumArray() {
-        final Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите длину массива: ");
-        int len = scanner.nextInt();
-        int arr[] = new int[len];
-        System.out.println("Введите диапазон значений массива: ");
-        int numbers = scanner.nextInt();
-        for (int i = 0; i < len; i++) {
-            arr[i] = random.nextInt(numbers);
-        }
-        System.out.println(Arrays.toString(arr));
+    public static boolean sumArray(int arr[]) {
         int leftSum;
         int rightSum;
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < arr.length; i++) {
             leftSum = 0;
             rightSum = 0;
             for (int j = 0; j < i; j++) {
                 leftSum += arr[j];
             }
-            for (int j = i; j < len; j++) {
+            for (int j = i; j < arr.length; j++) {
                 rightSum += arr[j];
             }
             if (rightSum == leftSum) return true;
@@ -170,4 +174,52 @@ public class HomeWork03 {
         return false;
 
     }
+
+    public static void offsetArr() {
+        final Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите длину массива: ");
+        int len = scanner.nextInt();
+        int arr[] = new int[len];
+        System.out.println("Введите количество элементов для переноса: ");
+        int changeArr = scanner.nextInt();
+        for (int i = 0; i < len; i++) {
+            arr[i] = random.nextInt(40);
+        }
+        System.out.println(Arrays.toString(arr));
+        int bubble, bubble2;
+        if (changeArr > 0) {
+            for (int j = 0; j < changeArr; j++) {
+                bubble2 = arr[0];
+                for (int i = 0; i < arr.length; i++) {
+                    if ((i + 1) > (len - 1)) {
+                        bubble = arr[i + 1 - len];
+                        arr[i + 1 - len] = bubble2;
+                        bubble2 = bubble;
+                    } else {
+                        bubble = arr[i + 1];
+                        arr[i + 1] = bubble2;
+                        bubble2 = bubble;
+                    }
+                }
+            }
+        } else {
+            for (int j = changeArr; j < 0; j++) {
+                bubble2 = arr[0];
+                for (int i = arr.length; i > 0; i--) {
+                    if ((i - 1) < 0) {
+                        bubble = arr[arr.length - 1];
+                        arr[arr.length - 1] = bubble2;
+                        bubble2 = bubble;
+                    } else {
+                        bubble = arr[i - 1];
+                        arr[i - 1] = bubble2;
+                        bubble2 = bubble;
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
 }
